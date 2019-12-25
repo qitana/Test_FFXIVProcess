4de84bee044382f2000f8b393060ed0f35f08368
qitana <53415856+qitana@users.noreply.github.com>
2019-12-25 22:26:35 +0900

    added CloseHandle

--- ConsoleApp_Success/Program.cs
+++ ConsoleApp_Success/Program.cs
@@ -9,13 +9,18 @@ namespace ConsoleApp_Success
         [DllImport("kernel32.dll")]
         public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
 
+        [DllImport("kernel32.dll")]
+        static extern int CloseHandle(IntPtr hObject);
+
         static void Main(string[] args)
         {
             Process[] processesByName = Process.GetProcessesByName("ffxiv_dx11");
             foreach (Process process in processesByName)
+
             {
                 IntPtr processHandle = OpenProcess(0x0010, false, process.Id);
                 Console.WriteLine(processHandle.ToInt64());
+                CloseHandle(processHandle);
             }
 
             Console.ReadLine();
